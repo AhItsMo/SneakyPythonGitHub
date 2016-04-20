6b869bb556eafc53d0e90bd3241d7ffa185bba52
DinaAli <DAli@integrant.com>
2016-04-20 18:54:11 +0200

    Updating NavigationBarMethods

--- NavigationBar/NavigationBarMethods.py
+++ NavigationBar/NavigationBarMethods.py
@@ -4,7 +4,7 @@ from NavigationBar.NavigationBarElements import NavigationBarElements
 class NavigationBarMethods:
 
     def sign_out_list_item_click(self):
-        # Verify that clicking the Sign in button will navigate the user to the Home Page
+        # Verify that clicking the profile drop down list and sign out list item will navigate the user to the Home Page
 
         self.driver.find_element(*NavigationBarElements.view_profile_drop_down).click()
         self.driver.find_element(*NavigationBarElements.sign_out_list_item).click()
