11700304c8db92abdd1c29a1bfa04f26be8f8fbf
DinaAli <DAli@integrant.com>
2016-04-20 19:24:11 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	NavigationBar/NavigationBarMethods.py

--- Methods/NavigationBarMethods.py
+++ Methods/NavigationBarMethods.py
@@ -3,9 +3,8 @@ from PageObjectModels.NavigationBarPageObject import NavigationBarPageObject
 
 class NavigationBarMethods:
 
+    #  Verify that clicking the profile drop down list and sign out list item will navigate the user to the Home Page
     def sign_out_list_item_click(self):
-
-        #  Verify that clicking the profile drop down list and sign out list item will navigate the user to the Home Page
 
         self.driver.find_element(*NavigationBarPageObject.view_profile_drop_down).click()
         self.driver.find_element(*NavigationBarPageObject.sign_out_list_item).click()
