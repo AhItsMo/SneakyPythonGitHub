09c09a6cfc4a766baa4fd94bccdfc55e90af4c7b
MAbdelMonem <MAbdelMonem@integrantcom>
2016-04-20 13:44:10 +0200

    Adding a Base Test Case and a Utilities folder.

--- Tests/HomeBeforeLoginTests.py
+++ Tests/HomeBeforeLoginTests.py
@@ -1,34 +1,25 @@
 import unittest
-
-from selenium import webdriver
-
+from Utilities.BaseTestCase import BaseTestCase
 from Methods.HomeBeforeLoginMethods import HomeBeforeLoginMethods
 from PageObjectModels.LoginPageObject import LoginPageObject
 
 
-class HomeTests(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.driver = webdriver.Firefox()
-        cls.driver.implicitly_wait(30)
-        cls.driver.maximize_window()
-        cls.driver.get("https://github.com/")
+class HomeTests(BaseTestCase):
 
-#   Verify that clicking the Login button will navigate the user to the Login Page.
     def test_login_button(self):
-#   Click Sign In button
+        #   Verify that clicking the Login button will navigate the user to the Login Page.
+
+        #   Navigate to github.com
+        self.driver.get("https://github.com/")
+
+        #   Click Sign In button
         HomeBeforeLoginMethods.click_sign_in_button(self)
 
-#       Assert that the Sign in to GitHub label is displayed
+        #   Assert that the Sign in to GitHub label is displayed
         self.assertTrue(self.driver.find_element(*LoginPageObject.sign_in_to_github_label).is_displayed())
 
-#       Navigate back to the Home Page
+        #   Navigate back to the Home Page
         self.driver.get("https://github.com")
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.driver.quit()
-
 
 
 if __name__ == '__main__':
