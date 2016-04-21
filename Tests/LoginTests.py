c36e2fe51607a06d4e27ba6f7d5037ad87c76680
MAbdelMonem <MAbdelMonem@integrantcom>
2016-04-21 14:34:55 +0200

    Adding the navigation to the web page

--- Tests/LoginTests.py
+++ Tests/LoginTests.py
@@ -10,13 +10,17 @@ from Methods.NavigationBarMethods import NavigationBarMethods
 class LoginTests(BaseTestCase):
 
     def test_sign_in(self):
-#        Execute the Login Method, in order to log in as DinaAli
+
+        #   Navigate to github.com
+        self.driver.get("https://github.com/login")
+
+        #   Execute the Login Method, in order to log in as DinaAli
         LoginMethods.sign_in_button(self, "DinaAli", "123@sta.com")
 
-#        Assert that the Navigation Bar is displayed, which means that login is successful.
+        #   Assert that the Navigation Bar is displayed, which means that login is successful.
         self.assertTrue(LoginMethods.is_profile_drop_down_displayed(self), "drop down menu is not displayed")
 
-#        Sign out, in order to continue testing from the start point (Login Page)
+        #   Sign out, in order to continue testing from the start point (Login Page)
         NavigationBarMethods.sign_out_list_item_click(self)
         self.driver.get("https://github.com/login")
 
