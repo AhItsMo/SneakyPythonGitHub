c54d3cfd0cd8d15c4ae7526e13ffe2b4c7fb7862
MAbdelMonem <MAbdelMonem@integrantcom>
2016-05-11 19:47:50 +0200

    Added a generic pre-test login method to BasePage file

--- Utilities/BasePage.py
+++ Utilities/BasePage.py
@@ -2,10 +2,14 @@ import xlrd
 from selenium.webdriver.common.by import By
 
 
+# noinspection PyBroadException
 class BasePage(object):
     #   Navigation Bar Locators
     view_profile_drop_down = (By.XPATH, "/html/body/div[2]/div/ul[2]/li[3]/a")
     sign_out_list_item = (By.XPATH, "/html/body/div[2]/div/ul[2]/li[3]/div/div/form/button")
+    username_box = (By.ID, "login_field")
+    password_box = (By.ID, "password")
+    sign_in_button = (By.XPATH, "/html/body/div[4]/div[1]/div/form/div[4]/input[3]")
 
     #   Properties
 
@@ -49,7 +53,16 @@ class BasePage(object):
         self.driver.close()
         self.driver.switch_to_window(self.driver.window_handles[0])
 
-    #
+    #   Pre-test conditional login (if the user is not already logged in)
+    def pretest_login(self):
+        try:
+            self.driver.find_element(*self.view_profile_drop_down).is_displayed()
+        except:
+            print("User is not logged in. Pre-test login is executed")
+            self.driver.get("https://github.com/login")
+            self.driver.find_element(*self.username_box).send_keys("sneakypythontestuser")
+            self.driver.find_element(*self.password_box).send_keys("000000aa")
+            self.driver.find_element(*self.sign_in_button).click()
 
     #   read data from a certain sheet in the Test Data workbook "SneakyPythonGitHubTestData.xlsx"
     @staticmethod
