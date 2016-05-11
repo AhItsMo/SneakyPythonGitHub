8e5de2949fd0ea574321de4041ac235813195e1a
MAbdelMonem <MAbdelMonem@integrantcom>
2016-05-11 17:28:53 +0200

    Updated Test Data Deteleted NavigationBarPageObject

--- PageObjectModels/NavigationBarPageObject.py
+++
@@ -1,6 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class NavigationBarPageObject:
-    view_profile_drop_down = (By.XPATH, "/html/body/div[2]/div/ul[2]/li[3]/a")
-    sign_out_list_item = (By.XPATH, "/html/body/div[2]/div/ul[2]/li[3]/div/div/form/button")
